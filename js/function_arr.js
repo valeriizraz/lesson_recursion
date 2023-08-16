392ee8a46bcadc2280c02965ff32bf4574154f13
valeriizraz <valeriizraz@gmail.com>
2023-08-16 23:15:42 +0200

    правки if

--- js/function_arr.js
+++ js/function_arr.js
@@ -1,22 +1,22 @@
-// 'use strict';
+'use strict';
 
-// const arr = [];
+const arr = [];
 
-// function recursFoo(arr) {
-//   const arrCopy = arr;
+function recursFoo(arr) {
+  const arrCopy = arr;
 
-//   const rand = Math.round(Math.random() * 10);
-//   arr.push(rand);
+  const rand = Math.round(Math.random() * 10);
+  arr.push(rand);
 
-//   const sum = arr.reduce((acc, curr) => acc += curr, 0);
-//   console.log(sum);
+  const sum = arr.reduce((acc, curr) => acc += curr, 0);
+  console.log(sum);
 
-//   if (sum < 50) {
-//     return recursFoo(arr);
-//   } else {
-//     return arrCopy;
-//   }
-// }
+  if (sum < 50) {
+    return recursFoo(arr);
+  } else {
+    return arrCopy;
+  }
+}
 
-// console.log(recursFoo(arr));
+console.log(recursFoo(arr));
 
